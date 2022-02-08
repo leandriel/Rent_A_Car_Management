b87d8c4cd9ff886d1177e3d8975668145c038a31
ValderasIncluit <leonardo.valderas@incluit.com>
2022-02-08 18:30:33 -0300

    Car repository done

---
+++ app/src/main/java/com/leandroid/system/rentacarmanagement/data/dto/CarDTO.kt
@@ -0,0 +1,24 @@
+package com.leandroid.system.rentacarmanagement.data.dto
+
+import com.leandroid.system.rentacarmanagement.model.Brand
+import com.leandroid.system.rentacarmanagement.model.Car
+
+data class CarDTO(
+    val car: Car,
+    val brands: MutableList<Brand>
+) {
+    constructor() : this(
+        Car(
+            "",
+            "",
+            "",
+            Brand(
+                "",
+                ""
+            ),
+            false,
+            ""
+        ),
+        mutableListOf()
+    )
+}
--- app/src/main/java/com/leandroid/system/rentacarmanagement/data/repository/CarRepository.kt
+++ app/src/main/java/com/leandroid/system/rentacarmanagement/data/repository/CarRepository.kt
@@ -1,6 +1,15 @@
 package com.leandroid.system.rentacarmanagement.data.repository
 
+import com.leandroid.system.rentacarmanagement.data.dto.CarDTO
+import com.leandroid.system.rentacarmanagement.data.utils.ApiResponse
+import com.leandroid.system.rentacarmanagement.data.utils.Response
+import com.leandroid.system.rentacarmanagement.model.Car
+import kotlinx.coroutines.flow.Flow
+
 interface CarRepository {
-    fun getCars()
-    fun getCar(id: String)
+    fun getCars(): Flow<Response<ApiResponse<MutableList<Car>>>>
+    fun getCar(id: String): Flow<Response<ApiResponse<CarDTO>>>
+    fun saveCar(car: Car): Flow<Response<ApiResponse<MutableList<Car>>>>
+    fun updateCar(car: Car): Flow<Response<ApiResponse<MutableList<Car>>>>
+    fun deleteCar(id: String): Flow<Response<ApiResponse<MutableList<Car>>>>
 }
