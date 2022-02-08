49d47572045aad711240d53b5b21829d5bb11b5a
leandriel <leandro88valderas@gmail.com>
2022-02-07 22:02:52 -0300

    fix reserve

--- app/src/main/java/com/leandroid/system/rentacarmanagement/model/Reserve.kt
+++ app/src/main/java/com/leandroid/system/rentacarmanagement/model/Reserve.kt
@@ -3,7 +3,7 @@ package com.leandroid.system.rentacarmanagement.model
 data class Reserve(
     val id: String,
     val car: Car,
-    val Fly: String,
+    val fly: String,
     val startDate: String,
     val endDate: String
 )
