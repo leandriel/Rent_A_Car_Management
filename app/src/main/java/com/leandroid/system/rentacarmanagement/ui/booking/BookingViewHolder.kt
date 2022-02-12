a12da1483cfb0ca28816295c4fb1b438e6e59662
leandriel <leandro88valderas@gmail.com>
2022-02-12 20:40:42 -0300

    making add view holder

--- app/src/main/java/com/leandroid/system/rentacarmanagement/ui/booking/BookingViewHolder.kt
+++ app/src/main/java/com/leandroid/system/rentacarmanagement/ui/booking/BookingViewHolder.kt
@@ -1,4 +1,21 @@
 package com.leandroid.system.rentacarmanagement.ui.booking
 
-class BookingViewHolder {
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.leandroid.system.rentacarmanagement.databinding.ItemBookingBinding
+import com.leandroid.system.rentacarmanagement.model.Car
+import com.leandroid.system.rentacarmanagement.ui.car.CarListener
+
+class BookingViewHolder (view: View, private val listener: CarListener) : RecyclerView.ViewHolder(view) {
+    private val binding = ItemBookingBinding.bind(view)
+
+    fun bind(car: Car) {
+        binding.root.setOnClickListener {
+            listener.onClick(car.id)
+        }
+
+        binding.tvDetailsBooking.text = car.carDetails
+        binding.tvComment.text = car.comment
+//        Picasso.get().load(movie.urlImage).into(binding.movieImage)
+    }
 }
