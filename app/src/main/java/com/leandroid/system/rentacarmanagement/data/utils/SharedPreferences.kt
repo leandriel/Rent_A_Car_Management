a002b81bcf61464a5c899aaf8468f58626867e6c
LeonardoValderas <l.v.bass@hotmail.com>
2022-04-03 21:26:49 -0300

    Preference user updated

--- app/src/main/java/com/leandroid/system/rentacarmanagement/data/utils/SharedPreferences.kt
+++ app/src/main/java/com/leandroid/system/rentacarmanagement/data/utils/SharedPreferences.kt
@@ -6,6 +6,10 @@ import kotlinx.coroutines.flow.Flow
 interface SharedPreferences {
     suspend fun saveUser(user: User): Boolean
     suspend fun saveToken(token: String) : Boolean
+    suspend fun saveUserId(id: String): Boolean
+    suspend fun saveCompanyId(id: String): Boolean
     suspend fun getUser(): Flow<User?>
     suspend fun getToken(): Flow<String>
+    suspend fun getUserId(): Flow<String>
+    suspend fun getCompanyId(): Flow<String>
 }
--- app/src/main/java/com/leandroid/system/rentacarmanagement/data/utils/SharedPreferencesImpl.kt
+++ app/src/main/java/com/leandroid/system/rentacarmanagement/data/utils/SharedPreferencesImpl.kt
@@ -8,7 +8,9 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
+import com.leandroid.system.rentacarmanagement.data.utils.SharedPreferencesImpl.PreferencesKeys.COMPANY_ID_KEY
 import com.leandroid.system.rentacarmanagement.data.utils.SharedPreferencesImpl.PreferencesKeys.TOKEN_KEY
+import com.leandroid.system.rentacarmanagement.data.utils.SharedPreferencesImpl.PreferencesKeys.USER_ID_KEY
 import com.leandroid.system.rentacarmanagement.data.utils.SharedPreferencesImpl.PreferencesKeys.USER_KEY
 import com.leandroid.system.rentacarmanagement.model.User
 import kotlinx.coroutines.flow.*
@@ -16,10 +18,12 @@ import kotlinx.coroutines.flow.*
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "rent_a_car_data")
 
 class SharedPreferencesImpl(private val context: Context) : SharedPreferences {
-    //private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = FILE_NAME)
 
     override suspend fun saveUser(user: User): Boolean {
         return try {
+            saveUserId(user.id)
+            saveCompanyId(user.company.id)
+            saveToken(user.token)
             val userJson = Gson().toJson(user)
             getDataStore().edit { data ->
                 data[USER_KEY] = userJson
@@ -35,6 +39,30 @@ class SharedPreferencesImpl(private val context: Context) : SharedPreferences {
         return try {
             getDataStore().edit { data ->
                 data[TOKEN_KEY] = token
+            }
+            true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+    }
+
+    override suspend fun saveUserId(id: String): Boolean {
+        return try {
+            getDataStore().edit { data ->
+                data[USER_ID_KEY] = id
+            }
+            true
+        } catch (e: Exception) {
+            e.printStackTrace()
+            false
+        }
+    }
+
+    override suspend fun saveCompanyId(id: String): Boolean {
+        return try {
+            getDataStore().edit { data ->
+                data[COMPANY_ID_KEY] = id
             }
             true
         } catch (e: Exception) {
@@ -69,16 +97,34 @@ class SharedPreferencesImpl(private val context: Context) : SharedPreferences {
             }
     }
 
+    override suspend fun getUserId(): Flow<String> {
+        return getDataStore().data
+            .map { preferences ->
+                preferences[USER_ID_KEY] ?: ""
+            }
+    }
+
+    override suspend fun getCompanyId(): Flow<String> {
+        return getDataStore().data
+            .map { preferences ->
+                preferences[COMPANY_ID_KEY] ?: ""
+            }
+    }
+
     private fun getDataStore() = context.dataStore
 
     private object PreferencesKeys {
         val USER_KEY = stringPreferencesKey(USER)
         val TOKEN_KEY = stringPreferencesKey(TOKEN)
+        val USER_ID_KEY = stringPreferencesKey(USER_ID)
+        val COMPANY_ID_KEY = stringPreferencesKey(COMPANY_ID)
     }
 
     companion object {
         private const val USER = "user"
         private const val TOKEN = "token"
+        private const val USER_ID = "user_id"
+        private const val COMPANY_ID = "company_id"
         private const val FILE_NAME = "rent_a_car_data"
     }
 }
